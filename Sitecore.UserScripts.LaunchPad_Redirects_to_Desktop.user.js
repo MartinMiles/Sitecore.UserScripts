51dd6b1dc37be011e628cf0c12182c14ab4a7a66
Martin Miles <miles@martin.by>
2019-05-31 18:08:31 +0100

    description affixed

--- Sitecore.UserScripts.LaunchPad_Redirects_to_Desktop.user.js
+++ Sitecore.UserScripts.LaunchPad_Redirects_to_Desktop.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Launchpad redirects to shell opening Content Editor
+// @name         Launchpad redirects to shell (Sitecore Desktop)
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
